fb8aa9916ed106eca581870711e0d946def7309f
Willian Ataides <sg552.santos@gmail.com>
2023-11-26 18:51:47 -0300

    Contato entities

---
+++ src/Aplicacao/Programa.java
@@ -0,0 +1,13 @@
+package Aplicacao;
+
+import model.entities.Contato;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Contato contato1 = new Contato(1, "Willian", "sg552.santos@gmail.com","av.roosevelt 770");
+		System.out.println(contato1);
+	}
+
+}
---
+++ src/model/entities/Contato.java
@@ -0,0 +1,85 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Contato implements Serializable {
+	
+
+	private static final long serialVersionUID = 1L;
+	private long id;
+	private String nome;
+	private String email;
+	private String endereco;
+	
+	public Contato() {
+		
+	}
+
+	public Contato(long i, String nome, String email, String endereco) {
+		this.id = i;
+		this.nome = nome;
+		this.email = email;
+		this.endereco = endereco;
+	}
+
+	public double getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Contato other = (Contato) obj;
+		return Double.doubleToLongBits(id) == Double.doubleToLongBits(other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Contato [id=" + id + ", nome=" + nome + ", email=" + email + ", endereco=" + endereco + "]";
+	}
+
+
+	
+	
+	
+	
+}
